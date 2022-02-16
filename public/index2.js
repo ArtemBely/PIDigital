e0d6e33493b19bb03307a643fdf435a3da19f98f
Артем Белышев <you@example.com>
2022-02-16 16:31:06 +0100

    bags1

--- public/index2.js
+++ public/index2.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+	var inTouch = document.getElementById('inTouch');
 	//E-mail Ajax Send
 	$("#order_our1").submit(function() { //Change
     swal({
@@ -15,12 +15,15 @@ $(document).ready(function() {
       console.log('done');
 			setTimeout(function() {
 				// Done Functions
+
 				th.trigger("reset");
 			}, 1000);
 		});
+
 		return false;
 	});
 });
+
 
 var box = document.getElementById('box1');
 var squiggle = document.querySelectorAll('.squiggle');
