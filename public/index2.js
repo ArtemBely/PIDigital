49c07b625eca8da94a7d88af6fbe129b328561f6
Артем Белышев <you@example.com>
2022-02-26 12:42:00 +0100

    mozilla bag

--- public/index2.js
+++ public/index2.js
@@ -47,18 +47,78 @@ window.addEventListener('scroll', () => {
   }
 });
 
+
+jQuery.preloadImages = function()
+ {
+  for(var i = 0; i < arguments.length; i++)
+  {
+   jQuery("<img>").attr("src", arguments[ i ]);
+  }
+ };
+
+//указываем путь к изображению, которое нужно подгрузить
+$.preloadImages("./public/images/lines/Lines100.png", "./public/images/lines/Lines101.png", "./public/images/lines/Lines102.png",
+"./public/images/lines/Lines103.png", "./public/images/lines/Lines104.png", "./public/images/lines/Lines105.png", "./public/images/lines/Lines106.png",
+"./public/images/lines/Lines107.png", "./public/images/lines/Lines108.png", "./public/images/lines/Lines109.png", "./public/images/lines/Lines110.png",
+"./public/images/lines/Lines111.png", "./public/images/lines/Lines112.png", "./public/images/lines/Lines113.png", "./public/images/lines/Lines114.png",
+"./public/images/lines/Lines115.png",
+"./public/images/lines/Lines116.png", "./public/images/lines/Lines117.png", "./public/images/lines/Lines118.png", "./public/images/lines/Lines119.png",
+"./public/images/lines/Lines120.png", "./public/images/lines/Lines121.png", "./public/images/lines/Lines122.png", "./public/images/lines/Lines123.png",
+"./public/images/lines/Lines124.png", "./public/images/lines/Lines125.png", "./public/images/lines/Lines126.png", "./public/images/lines/Lines127.png",
+"./public/images/lines/Lines128.png", "./public/images/lines/Lines129.png", "./public/images/lines/Lines130.png", "./public/images/lines/Lines131.png",
+"./public/images/lines/Lines132.png",
+"./public/images/lines/Lines133.png", "./public/images/lines/Lines134.png", "./public/images/lines/Lines135.png", "./public/images/lines/Lines136.png",
+"./public/images/lines/Lines137.png",
+"./public/images/lines/Lines138.png", "./public/images/lines/Lines139.png", "./public/images/lines/Lines140.png", "./public/images/lines/Lines141.png",
+"./public/images/lines/Lines142.png", "./public/images/lines/Lines143.png", "./public/images/lines/Lines144.png", "./public/images/lines/Lines145.png",
+"./public/images/lines/Lines146.png", "./public/images/lines/Lines147.png", "./public/images/lines/Lines148.png", "./public/images/lines/Lines149.png",
+"./public/images/lines/Lines150.png", "./public/images/lines/Lines151.png", "./public/images/lines/Lines152.png");
+
 var lines = document.getElementById('lines1');
 var num = 99;
+var fps = 50;
+
 
 function secvention() {
   num++;
   var source = `./public/images/lines/Lines${num}.png`;
   lines.src = source;
-  if(num < 152) {
-    requestAnimationFrame(secvention);
-  }
+	if(num < 152) {
+			setTimeout(() => {
+					ret = requestAnimationFrame(secvention);
+			}, 1000/fps);
+	}
 }
 
-window.addEventListener('load', () => {
-  secvention();
-});
+window.onload = function () {
+			secvention();
+}
+
+
+/*
+var app1 = document.getElementById('app1');
+var num = 99;
+var fps = 10;
+
+function secvention() {
+  num++;
+  var source = `./public/images/lines/Lines${num}.png`;
+		if(num < 152) {
+			setTimeout(() => {
+				if(num > 99) {
+					var fd = document.getElementById(`ids${num - 1}`);
+					app1.removeChild(fd);
+				}
+
+					var e = new Image();
+					e.src = source;
+					e.id = `ids${num}`
+				  app1.appendChild(e);
+					ret = requestAnimationFrame(secvention);
+			}, 1000/fps);
+		}
+}
+*/
+window.onload = function () {
+			secvention();
+}
